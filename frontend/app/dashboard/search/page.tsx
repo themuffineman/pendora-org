88538a02078682a3cbcb4d45e5a2ddc182888426
Petrus <petrusheya@gmail.com>
2024-09-04 23:02:17 +0200

    Added search

--- frontend/app/dashboard/search/page.tsx
+++ frontend/app/dashboard/search/page.tsx
@@ -1,19 +1,77 @@
+"use client"
 import AdCard from '@/components/AdCard'
-
-import React from 'react'
+import { useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
+    const [ads, setAds] = useState<string[]>([''])
+    const [failedToFetch, setFailedToFetch] = useState<boolean>(false)
+    const [isFetching, setIsFetching] = useState<boolean>(false)
+    const searchParams = useSearchParams()
+    const [url, setUrl] = useState<string | null>(null)
+
+    useEffect(() => {
+        const paramUrl = searchParams.get('url'); // Get the "url" parameter
+        if (paramUrl !== url) {  // Check if it's different from the current state
+            setUrl(paramUrl);    // Update the state
+        }
+    }, [searchParams, url]);
+
+    useEffect(() => {
+        if (url) {
+            fetchData();
+        }
+    }, [url]);
+    async function fetchData(){
+        for(let retries = 0; retries < 4; retries++){
+            try {
+                setIsFetching(true)
+                const adResponse = await fetch('https://pendora-org.onrender.com/api/get-google-ads', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        url: 'stripe.com'
+                    })
+                })
+                if(!adResponse){
+                    continue
+                }
+                const ads = await adResponse.json()
+                setAds(ads.adImages)
+                setIsFetching(false)
+                
+            } catch (error) {
+                if(retries === 3 ){
+                    setIsFetching(false)
+                    setFailedToFetch(true)
+                }else{
+                    continue
+                }
+            }
+        }
+    }
     return (
         <> 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 place-items-center  grid-flow-row w-full h-full'>
-                <AdCard adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' adImage='/8455966320107134295.png'/>  
-                <AdCard adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' adImage='/8455966320107134295.png'/>  
-                <AdCard adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' adImage='/8455966320107134295.png'/>  
-                <AdCard adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' adImage='/8455966320107134295.png'/>  
-                <AdCard adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' adImage='/8455966320107134295.png'/>  
-                <AdCard adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' adImage='/8455966320107134295.png'/>  
-                <AdCard adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' adImage='/8455966320107134295.png'/>  
-                <AdCard adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' adImage='/8455966320107134295.png'/>
+                {
+                    ads?.length > 0 ? (
+                        ads.map((src) => (
+                            <AdCard 
+                                key={src}
+                                adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' 
+                                adImage={src} 
+                            />  
+                        ))
+                    ) : failedToFetch ? (
+                        <div 
+                            onClick={fetchData} 
+                            className='w-max p-2 bg-[#f5f5f5] transition flex items-center justify-center rounded-md text-black text-sm hover:bg-[#f0f0f0]'
+                        >
+                            Failed To Fetch. Try Again
+                        </div>
+                    ) : isFetching && (
+                        <div className='size-20 animate-spin border-2 border-t-0 border-black'/>
+                    )
+                }
             </div>   
         </>
     )
