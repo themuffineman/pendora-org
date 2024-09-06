0bdd4488c334ef2429a3b83dfe9aefffa790b427
Petrus <petrusheya@gmail.com>
2024-09-06 18:43:52 +0200

    Added auth

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -6,6 +6,7 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { usePathname } from 'next/navigation';
+import Search from '@/components/Search';
 
 interface componentProps {
     children: React.ReactNode;
@@ -19,16 +20,13 @@ const layout: React.FC<componentProps> = ({ children }) => {
         <nav className="w-full px-2 bg-white flex items-center justify-end py-2 gap-10 border-b border-[#F5F5F5] shadow-lg">
             {
                 pathname === '/dashboard/search' ? (
-                    <form className='relative w-max flex justify-center items-center self-start'>
-                        <input className='w-[30rem] h-12 p-2 px-[40px] bg-[#F5F5F5] placeholder:text-black/30 rounded-md ' placeholder='Enter new website domain e.g. domain.com' type="search" />
-                        <svg className='absolute left-[5px] top-1/2 -translate-y-1/2' xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#d3d3d3" viewBox="0 0 256 256"><path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path></svg>
-                    </form>
+                    <Search/>
                 ):(
                     ''
                 )
             }
             <button className='w-max px-4 p-2 rounded-md text-black flex items-center justify-center bg-yellow-200'>Upgrade</button>
-            <Popover>
+            {/* <Popover>
                 <PopoverTrigger className='p-2 rounded-md hover:bg-[#F5F5F5] relative'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000000" viewBox="0 0 256 256"><path d="M221.8,175.94C216.25,166.38,208,139.33,208,104a80,80,0,1,0-160,0c0,35.34-8.26,62.38-13.81,71.94A16,16,0,0,0,48,200H88.81a40,40,0,0,0,78.38,0H208a16,16,0,0,0,13.8-24.06ZM128,216a24,24,0,0,1-22.62-16h45.24A24,24,0,0,1,128,216ZM48,184c7.7-13.24,16-43.92,16-80a64,64,0,1,1,128,0c0,36.05,8.28,66.73,16,80Z"></path></svg>
                     <div className='size-2 rounded-full bg-yellow-300 animate-pulse absolute top-2 right-3'/>
@@ -38,15 +36,13 @@ const layout: React.FC<componentProps> = ({ children }) => {
                   <p className='p-3 text-black text-sm hover:bg-[#f1f1f1] rounded-md w-full h-max cursor-pointer'>Get 50% off</p>
                   <p className='p-3 text-black text-sm hover:bg-[#f1f1f1] rounded-md w-full h-max cursor-pointer'>Get 50% off</p>
                 </PopoverContent>
-            </Popover>
-            <div className='p-2 rounded-md hover:bg-[#F5F5F5] animate-[transform:2px] relative'>
-            </div>
+            </Popover> */}
             <div className='flex items-center justify-between w-[7rem] p-3 rounded-md bg-[#F5F5F5]'>
                 <div className='p-2 font-bold size-8 rounded-full bg-black text-white flex items-center justify-center'>P</div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"><path d="M181.66,170.34a8,8,0,0,1,0,11.32l-48,48a8,8,0,0,1-11.32,0l-48-48a8,8,0,0,1,11.32-11.32L128,212.69l42.34-42.35A8,8,0,0,1,181.66,170.34Zm-96-84.68L128,43.31l42.34,42.35a8,8,0,0,0,11.32-11.32l-48-48a8,8,0,0,0-11.32,0l-48,48A8,8,0,0,0,85.66,85.66Z"></path></svg>
             </div>
         </nav>
-        <div className='w-full p-20 h-full flex flex-col gap-[2rem] justify-center items-center overflow-auto bg-white'>
+        <div className='w-full h-full flex flex-col p-10 gap-[2rem] justify-start items-center overflow-auto bg-white'>
             {children}
         </div>
     </main>
--- frontend/app/dashboard/search/page.tsx
+++ frontend/app/dashboard/search/page.tsx
@@ -2,48 +2,52 @@
 import AdCard from '@/components/AdCard'
 import { useSearchParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
 const page = () => {
-    const [ads, setAds] = useState<string[]>([''])
+    const [ads, setAds] = useState<string[] | []>([])
     const [failedToFetch, setFailedToFetch] = useState<boolean>(false)
     const [isFetching, setIsFetching] = useState<boolean>(false)
     const searchParams = useSearchParams()
-    const [url, setUrl] = useState<string | null>(null)
+    const url = searchParams.get("url")
 
     useEffect(() => {
-        const paramUrl = searchParams.get('url'); // Get the "url" parameter
-        if (paramUrl !== url) {  // Check if it's different from the current state
-            setUrl(paramUrl);    // Update the state
-        }
-    }, [searchParams, url]);
-
-    useEffect(() => {
-        if (url) {
-            fetchData();
-        }
-    }, [url]);
+        console.log('running yeah')
+        fetchData();
+    },[]);
     async function fetchData(){
+        setFailedToFetch(false)
+        setIsFetching(true)
         for(let retries = 0; retries < 4; retries++){
             try {
-                setIsFetching(true)
                 const adResponse = await fetch('https://pendora-org.onrender.com/api/get-google-ads', {
                     method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
                     body: JSON.stringify({
-                        url: 'stripe.com'
+                        url: url
                     })
                 })
-                if(!adResponse){
+                if(!adResponse.ok){
                     continue
                 }
                 const ads = await adResponse.json()
                 setAds(ads.adImages)
                 setIsFetching(false)
-                
+                break
             } catch (error) {
                 if(retries === 3 ){
                     setIsFetching(false)
                     setFailedToFetch(true)
                 }else{
+                    console.log('Retrying request')
                     continue
                 }
             }
@@ -51,28 +55,53 @@ const page = () => {
     }
     return (
         <> 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 place-items-center  grid-flow-row w-full h-full'>
-                {
-                    ads?.length > 0 ? (
-                        ads.map((src) => (
-                            <AdCard 
-                                key={src}
-                                adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' 
-                                adImage={src} 
-                            />  
-                        ))
-                    ) : failedToFetch ? (
-                        <div 
-                            onClick={fetchData} 
-                            className='w-max p-2 bg-[#f5f5f5] transition flex items-center justify-center rounded-md text-black text-sm hover:bg-[#f0f0f0]'
-                        >
-                            Failed To Fetch. Try Again
-                        </div>
-                    ) : isFetching && (
-                        <div className='size-20 animate-spin border-2 border-t-0 border-black'/>
-                    )
-                }
-            </div>   
+            <div className='flex flex-col w-full h-full items-center justify-start'>
+                <div className='w-full px-4 flex items-cetner justify-between'>
+                    <div className='text-xl font-semibold text-black tracking-tight'>
+                        Results Found: {ads.length}
+                    </div>
+                    <Select defaultValue='30Days'>
+                        <SelectTrigger className="w-max h-12 flex gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H184V24a8,8,0,0,0-16,0v8H88V24a8,8,0,0,0-16,0v8H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM72,48v8a8,8,0,0,0,16,0V48h80v8a8,8,0,0,0,16,0V48h24V80H48V48ZM208,208H48V96H208V208Z"></path></svg>
+                            <SelectValue placeholder="Choose Date Range" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="30Days">
+                                <div className='flex gap-2 items-center justify-between '>
+                                    <span className='text-base font-medium text-center'>Past 30 Days</span>
+                                </div>
+                            </SelectItem>
+                            <SelectItem value="60Days">
+                                <div className='flex gap-2 items-center justify-between'>
+                                    <span className='text-base font-medium text-center'>Past 60 Days</span>
+                                </div>
+                            </SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div className={` ${failedToFetch || isFetching ? 'flex items-center justify-center': 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 place-items-center  grid-flow-row'}  w-full h-full`}>
+                    {
+                        ads?.length > 0 ? (
+                            ads.map((src) => (
+                                <AdCard 
+                                    key={src}
+                                    adCopy='lorem ipsum lorem ipsum lorem ipsum lorem lorem ipsum' 
+                                    adImage={src} 
+                                />  
+                            ))
+                        ) : failedToFetch ? (
+                            <button 
+                                onClick={fetchData} 
+                                className='w-max p-4 bg-[#f5f5f5] transition flex items-center place-self-center justify-center rounded-md text-black text-base hover:bg-[#f0f0f0] shadow-2xl shadow-[#f2f2f2]'
+                            >
+                                Failed To Fetch. Try Again
+                            </button>
+                        ) : isFetching && (
+                            <div className='size-16 animate-spin rounded-full border-[5px] border-t-white border-[#d8d8d8]'/>
+                        )
+                    }
+                </div>
+            </div>
         </>
     )
 }
