57fd540cea370222a20f0ae51b4ffc1b06bdabcc
Petrus <petrusheya@gmail.com>
2024-09-08 19:41:57 +0200

    Added btn click

--- backend/index.mjs
+++ backend/index.mjs
@@ -14,8 +14,10 @@ app.use(express.json());
 app.post('/api/get-google-ads', async (req,res)=>{
     const {url} = req.body
     console.log('Received request:', url)
+
     const gridSelector = 'body > div:nth-child(9) > root > start-page > creative-grid > priority-creative-grid'
     const adCardSelector = 'body > div:nth-child(9) > root > start-page > creative-grid > priority-creative-grid > creative-preview'
+    const allAdsButton = 'body > div:nth-child(9) > root > start-page > creative-grid > material-button > material-ripple'
     let browser
     let page
     for(let browserRetries = 0; browserRetries < 4; browserRetries++){
@@ -44,9 +46,8 @@ app.post('/api/get-google-ads', async (req,res)=>{
             }
         }
     }
-    try {
-        await page.goto(`https://adstransparency.google.com/?region=US&domain=${url}&preset-date=Last+30+days`);
-        
+    try{
+        await page.goto(`https://adstransparency.google.com/?region=US&domain=${url}&preset-date=Last+30+days`)
         try {
             await page.waitForSelector(gridSelector);
             console.log('Selector Found')
@@ -54,15 +55,29 @@ app.post('/api/get-google-ads', async (req,res)=>{
             console.log('Grid selector not found');
             return res.sendStatus(404);
         }
-    
+        try{
+            await page.waitForSelector(allAdsButton)
+            await page.click(allAdsButton)
+            console.log('Clicked Button')
+        }catch(error){
+            console.log('Unable to click button')
+        }
+        try{
+            await page.waitForResponse(response =>
+                response.url().includes('https://displayads-formats.googleusercontent.com/ads/preview/content.js') && response.status() === 200
+            );
+            console.log('Response complete')
+        }catch(error){
+            console.log('Error waiting for response')
+        }
         const adGrid = await page.$(gridSelector);
         if (!adGrid) {
-            return res.json({ adImages: [] }).status(200); 
+            return res.json({ adImages: [] }).status(200)
         }
         console.log('Grid found')
         const adCards = await adGrid.$$(adCardSelector);
         const adImages = [];
-        for (const card of adCards) {
+        for(const card of adCards){
             try {
                 const adImage = await card.$('img');
                 if (adImage) {
@@ -75,7 +90,6 @@ app.post('/api/get-google-ads', async (req,res)=>{
                 continue;
             }
         }
-    
         return res.status(200).json({ adImages });
     } catch (error) {
         console.error('Error fetching ads:', error);
