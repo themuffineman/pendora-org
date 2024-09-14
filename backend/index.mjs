639dc39a74ca65e826159f6d82338c1af482f921
Petrus <petrusheya@gmail.com>
2024-09-14 13:36:27 +0200

    Added new origin

--- backend/index.mjs
+++ backend/index.mjs
@@ -9,7 +9,9 @@ app.listen(8080, ()=>{
     console.log('Server running')
 })
 app.use(cors({
-    origin: '*'
+    origin: 'http://localhost:3000', 
+    methods: 'GET,POST,PUT,DELETE',      
+    credentials: true
 }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
