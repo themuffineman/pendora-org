cab62c858d740c7e2e9c0d44c0d8cd027e5f19ed
Petrus <131905449+themuffineman@users.noreply.github.com>
2024-10-14 19:56:52 +0200

    Update AdCard.tsx Saving

--- frontend/components/AdCard.tsx
+++ frontend/components/AdCard.tsx
@@ -187,10 +187,10 @@ const AdCard: React.FC<componentProps> = ({adImage, type, format})=>{
             }
             {
                 type === "search" ? (
-                    <div className='w-full h-max p-1 px-1 bg-[#f5f5f5] text-sm truncate rounded-b-md flex items-center justify-between'>
-                        <button onClick={()=> {saveAd()}} className="p-2 rounded-md hover:bg-[#ffff] text-black">
+                    <div className='w-full h-max p-1 px-1 bg-[#f5f5f5] text-sm truncate rounded-b-md flex items-center justify-end'>
+{/*                         <button onClick={()=> {saveAd()}} className="p-2 rounded-md hover:bg-[#ffff] text-black">
                             Save to list
-                        </button>
+                        </button> */}
                         <div onClick={()=> triggerDownloadImage(adImage, 'Ads by AdsInspect')} className='w-max p-1 rounded-md hover:bg-[#ffff]'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
                         </div>
