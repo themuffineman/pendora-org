54120f4c121eb749c8d2fe6ab8cc5e24da9eb8b8
Petrus <131905449+themuffineman@users.noreply.github.com>
2024-11-02 01:15:09 +0000

    removed page.css

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Link from "next/link";
 import {useEffect, useState} from 'react'
-import "./page.css";
 import MainSearch from "@/components/MainSearch";
 import GetPro from "@/components/GetPro";
 import AuthLinks from "@/components/AuthLinks";
